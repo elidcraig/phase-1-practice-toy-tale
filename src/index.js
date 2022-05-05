59abf714d10830b1a63b921c15b96eefe437868d
Eli Craig <elidcraig@gmail.com>
2022-05-05 14:25:23 -0700

    cards rendered to DOM

--- src/index.js
+++ src/index.js
@@ -10,7 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(toysArr => toysArr.forEach(toyObj => renderOneCard(toyObj)))
 
   function renderOneCard(toyObj) {
-    console.log(toyObj)
+    
+    const toyCard = document.createElement('div')
+    const toyHeader = document.createElement('h2')
+    const toyImage = document.createElement('img')
+    const toyLikes = document.createElement('p')
+    const likeButton = document.createElement('button')
+
+    toyCard.className = 'card'
+    toyImage.className = 'toy-avatar'
+    likeButton.className = 'like-btn'
+    likeButton.id = toyObj.id
+
+    toyHeader.textContent = toyObj.name 
+    toyImage.src = toyObj.image 
+    toyLikes.textContent = `${toyObj.likes} Likes`
+    likeButton.textContent = 'Like ♥️'
+
+    toyCard.appendChild(toyHeader)
+    toyCard.appendChild(toyImage)
+    toyCard.appendChild(toyLikes)
+    toyCard.appendChild(likeButton)
+    toyCollectionContainer.appendChild(toyCard)
   }
 
   addBtn.addEventListener("click", () => {
