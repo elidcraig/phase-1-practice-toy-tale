04f0adf4efb542ddc359f16bc35685d9dcea0e1a
Eli Craig <elidcraig@gmail.com>
2022-05-05 07:47:56 -0700

    initial fetch, started helper function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollectionContainer = document.querySelector('#toy-collection')
+
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toysArr => toysArr.forEach(toyObj => renderOneCard(toyObj)))
+
+  function renderOneCard(toyObj) {
+    console.log(toyObj)
+  }
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +22,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
